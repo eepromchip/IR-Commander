ed3c7250744ca01c7e342713eb215beaee1550b7
EEPROM <eepromchip@gmail.com>
2018-11-27 21:01:35 -0500

    Moved out of SRC

--- IR Commander/IR_Commander.ino
+++ IR Commander/IR_Commander.ino
@@ -4,10 +4,12 @@
 IRsend irsend ;
 int pushButton = 2;
 
+
 void setup( )
 {
 pinMode(pushButton, INPUT);
-pushButton == LOW; 
+pinMode(LED_BUILTIN, OUTPUT);
+//pushButton == LOW;
 }
 
 void loop( ) {
@@ -15,6 +17,11 @@ void loop( ) {
       /* if the button is pushed */
       irsend.sendNEC(0x20DF10EF, 32); // This command is  POWER for Vizio
       irsend.sendNEC(0x61A0F00F, 32); // This command is POWER for Insignia
+         digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+         delay(1000);
+         digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+         delay(1000);                       // wait for a second
+
       //insert BT Soundbar power command here
 
    } else {
