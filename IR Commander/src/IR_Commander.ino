826b5950a189be38e888cf8562d5d27de96d62b6
EEPROM <eepromchip@gmail.com>
2018-11-27 20:25:17 -0500

    IR Command ready to deploy

---
+++ IR Commander/src/IR_Commander.ino
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <IRremote.h>
+
+IRsend irsend ;
+int pushButton = 2;
+
+void setup( )
+{
+pinMode(pushButton, INPUT);
+
+}
+
+void loop( ) {
+   if (pushButton == HIGH) {
+      /* if the button is pushed */
+      irsend.sendNEC(0x20DF10EF, 32); // This command is  POWER for Vizio
+      irsend.sendNEC(0x61A0F00F, 32); // This command is POWER for Insignia
+      //insert BT Soundbar power command here
+
+   } else {
+
+   }
+
+
+
+}//
